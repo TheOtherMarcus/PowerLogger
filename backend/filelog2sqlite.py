03bc40161870a7f701e22212b417c6f249660694
maran <maran@kornet1.se>
2018-07-12 21:30:57 +0200

    Correct timestamp for sqlite.

--- backend/filelog2sqlite.py
+++ backend/filelog2sqlite.py
@@ -8,7 +8,7 @@ import sqlite3
 
 logdir=sys.argv[1]
 
-conn = sqlite3.connect(logdir + ".test_sqlite")
+conn = sqlite3.connect(logdir + ".conv_sqlite")
 c = conn.cursor()
 c.execute("create table if not exists blink (time integer unique)")
 conn.commit()
@@ -17,9 +17,9 @@ c = conn.cursor()
 c = conn.cursor()
 i = 0
 for s in blinks(logdir):
-    print repr(s) + " " + str(datetime.fromtimestamp(s))
+    print str(int(s*1000)) + " " + str(datetime.fromtimestamp(s))
     try:
-        c.execute("insert into blink values (?)", (s,))
+        c.execute("insert into blink values (?)", (int(s*1000),))
     except sqlite3.Error as e:
         print "Database error: %s" % e
     if i % 1000 == 0:
